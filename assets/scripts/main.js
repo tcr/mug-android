6c6b28d830548a12ff49a598e9d5ada2cbf93c01
Tim Cameron Ryan <dev@tim-ryan.com>
2011-01-01 12:36:26 -0500

    Updated with compiled version of Mug for much quicker turn-around.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,12 +8,12 @@ exports.getMainActivity = function () {
 		onCreate: function (activity, bundle) {
 			var view = new TextView(activity);
 			activity.setContentView(view);
-			view.append("Hello Android!");
+			view.append("Welcome to Mug on Android!");
 			view.setBackgroundColor(0x88FFFF00);
 			
 			var toast = Toast.makeText(
 				activity.getApplicationContext(),
-				"Welcome to JavaScript on Android.",
+				"This is an alert!",
 				Toast.LENGTH_SHORT
 			);
 			toast.show();
